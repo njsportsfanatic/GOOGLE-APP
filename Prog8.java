f0a15c7de556242401fb3c600ce8616d350a0df3
njsportsfanatic <Mark@dyn139072.cc.lehigh.edu>
2015-11-30 20:43:32 -0500

    google app

---
+++ Prog8.java
@@ -0,0 +1,17 @@
+/*
+CSE 17
+Mark Quentzel
+maq218
+Program Description: create a 15 puzzle using GUI
+Program #8
+ */
+
+package prog8;
+
+public class Prog8 {
+
+    public static void main(String[] args) {
+        Puzzle.main(args);
+    }
+    
+}
